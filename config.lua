1bd3c7534ed081c4ca806e62553c15bf8aa27a57
ricx <66686454+zelbeus@users.noreply.github.com>
2021-05-27 20:25:58 +0200

    Create config.lua

---
+++ config.lua
@@ -0,0 +1,13 @@
+Config = {}
+
+Config.SellHorse = {
+    ["MinPrice"] = 100,
+    ["MaxPrice"] = 200,
+    ["Messages"] = {
+        ["Duration"] = 4000, --4sec
+        ["Title"] = "Horse Shop",
+        ["GetHorse"] = "~h~~e~Break in a Wild Horse and bring it Back here!",
+        ["NotBroken"] = "~h~~e~Bring here the last broken horse!",
+        ["Sold"] = "~h~~e~Horse Sold! +$",
+    }
+}
