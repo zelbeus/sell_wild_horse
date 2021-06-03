ab651dbd2dca5ee0c2f8cd59e611fb3d81626b53
ricx <66686454+zelbeus@users.noreply.github.com>
2021-06-03 08:10:26 +0200

    Update client.lua
    Blips Added
    Notification Added
    Some bug fixed
    Players can sell now horses at the given coords, line 30-33

--- client.lua
+++ client.lua
@@ -5,6 +5,39 @@
 local SellWildHorse = 0
 local Broken = 2
 
+local MapBlips = {
+    { name = 'Sell Wild Horse', sprite = -1103135225, x=-860.012, y=-1381.414, z=42.57}, --
+    { name = 'Sell Wild Horse', sprite = -1103135225, x=-5522.55, y=-3029.57, z=-3.215}, --
+    { name = 'Sell Wild Horse', sprite = -1103135225, x=-384.74,y= 783.82, z= 114.86}, --
+    { name = 'Sell Wild Horse', sprite = -1103135225, x=972.66, y=-1842.48, z=45.60}, --
+}
+
+Citizen.CreateThread(function()
+    for i, v in pairs(MapBlips) do
+        if v.name ~= nil then
+            local blip = N_0x554d9d53f696d002(1664425300, v.x, v.y, v.z)
+            SetBlipSprite(blip, v.sprite, 1)
+            SetBlipScale(blip, 0.2)
+            Citizen.InvokeNative(0x9CB1A1623062F402, blip, v.name)
+        end
+    end
+end)
+
+Citizen.CreateThread(function()
+    while true do
+        Citizen.Wait(20)
+        local coords = GetEntityCoords(PlayerPedId())
+        if (GetDistanceBetweenCoords(coords.x, coords.y, coords.z, -860.012, -1381.414, 42.57, true) < 2.0) or
+            (GetDistanceBetweenCoords(coords.x, coords.y, coords.z, -5522.550, -3029.578, -3.215, true) < 2.0) or
+            (GetDistanceBetweenCoords(coords.x, coords.y, coords.z, -384.741, 783.828, 114.86, true) < 2.0) or
+            (GetDistanceBetweenCoords(coords.x, coords.y, coords.z, 972.665, -1842.48, 45.60, true) < 2.0) then
+                if Citizen.InvokeNative(0x580417101DDB492F, 0, 0xD8F73058) then
+                    SellWildHorse()
+                end
+            end
+    end
+end)
+
 function SellWildHorse()
     if SellWildHorse ~= 0 then
         local PlayerPed = PlayerPedId()
@@ -15,16 +48,12 @@ function SellWildHorse()
             SellWildHorse= 0
             local r = math.random(Config.SellHorse.MinPrice,Config.SellHorse.MaxPrice)
             TriggerServerEvent("ricx_wildhorse:sold",r)
-            print("Sold - Price: "..r)
-            --TriggerEvent("redem_roleplay:NotifyLeft", Config.SellHorse.Messages.Title, Config.SellHorse.Messages.Sold..""..r, 'menu_textures', 'menu_icon_alert', Config.SellHorse.Messages.Duration)
-            else
-                print("Horse is not the borken one")
-                --TriggerEvent("redem_roleplay:NotifyLeft", Config.SellHorse.Messages.Title, Config.SellHorse.Messages.NotBroken, 'menu_textures', 'menu_icon_alert', Config.SellHorse.Messages.Duration)
-            end
-        end
+	    TriggerEvent("Notification:left", Config.SellHorse.Messages.Title, Config.SellHorse.Messages.Sold..""..r, 'menu_textures', 'menu_icon_alert',  Config.SellHorse.Messages.Duration)		
+          else
+               TriggerEvent("Notification:left", Config.SellHorse.Messages.Title, Config.SellHorse.Messages.NotBroken, 'menu_textures', 'menu_icon_alert',  Config.SellHorse.Messages.Duration)
+           end
     else
-        print("Get a wild horse")
-        --TriggerEvent("redem_roleplay:NotifyLeft", Config.SellHorse.Messages.Title, Config.SellHorse.Messages.GetHorse, 'menu_textures', 'menu_icon_alert', Config.SellHorse.Messages.Duration)
+        TriggerEvent("Notification:left", Config.SellHorse.Messages.Title, Config.SellHorse.Messages.GetHorse, 'menu_textures', 'menu_icon_alert',  Config.SellHorse.Messages.Duration)
     end
 end
 
@@ -53,4 +82,20 @@ Citizen.CreateThread(function()
 			end
 		end
 	end
+end)
+--Notification
+RegisterNetEvent('Notification:left')
+AddEventHandler('Notification:left', function(t1, t2, dict, txtr, timer)
+    if not HasStreamedTextureDictLoaded(dict) then
+        RequestStreamedTextureDict(dict, true) 
+        while not HasStreamedTextureDictLoaded(dict) do
+            Wait(5)
+        end
+    end
+    if txtr ~= nil then
+        exports.ricx_wild_horse.LeftNot(0, tostring(t1), tostring(t2), tostring(dict), tostring(txtr), tonumber(timer))
+    else
+        local txtr = "tick"
+        exports.ricx_wild_horse.LeftNot(0, tostring(t1), tostring(t2), tostring(dict), tostring(txtr), tonumber(timer))
+    end
 end)
