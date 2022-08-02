927d6e6fcfeeecefdb72588884926ed9a3492375
ricx <66686454+zelbeus@users.noreply.github.com>
2022-08-02 07:46:31 +0200

    Update server.lua

--- server.lua
+++ server.lua
@@ -3,9 +3,4 @@ AddEventHandler("ricx_wildhorse:sold", function(am)
 	local _source = source
 	print("Money got: "..am)
 	print("ADD YOUR OWN LOGIC FOR MONEY")
-	--[[ --RedEMRP money add--
-	TriggerEvent('redemrp:getPlayerFromId', _source, function(user)
-		user.addMoney(am)
-	end)
-	]]
 end)
