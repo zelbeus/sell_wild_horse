aedfb9cb4fdd68359d3287f633e6ec2a0f671f8c
ricx <66686454+zelbeus@users.noreply.github.com>
2022-08-02 07:55:43 +0200

    Update server.lua

--- server.lua
+++ server.lua
@@ -1,13 +1,26 @@
+local VorpCore
+if Config.Framework == "vorp" then 
+	TriggerEvent("getCore",function(core)
+	    VorpCore = core
+	end)
+end
+
+VorpInv = exports.vorp_inventory:vorp_inventoryApi()	
+end
+
 RegisterServerEvent("ricx_wildhorse:sold")
 AddEventHandler("ricx_wildhorse:sold", function()
 	local _source = source
 	local am = math.random(Config.SellHorse.MinPrice,Config.SellHorse.MaxPrice)
 	if Config.Framework == "redemrp" then 
-			
+		TriggerEvent('redemrp:getPlayerFromId', _source, function(user)
+		    user.addMoney(am)
+		end)	
 	elseif Config.Framework == "vorp" then 
-			
+		local Character = VorpCore.getUser(_source).getUsedCharacter
+		Character.addCurrency(0 , am)
 	elseif Config.Framework == "qbr" then 
 		local User = exports['qbr-core']:GetPlayer(_source)
-		User.Functions.AddMoney("cash", 100, "desc")
+		User.Functions.AddMoney("cash", am, "desc")
 	end
 end)
