4404a3d66ee192278e47d45aab94dd6ed3a94e96
ricx <66686454+zelbeus@users.noreply.github.com>
2022-08-02 07:54:06 +0200

    Update server.lua

--- server.lua
+++ server.lua
@@ -1,6 +1,13 @@
 RegisterServerEvent("ricx_wildhorse:sold")
-AddEventHandler("ricx_wildhorse:sold", function(am)
+AddEventHandler("ricx_wildhorse:sold", function()
 	local _source = source
-	print("Money got: "..am)
-	print("ADD YOUR OWN LOGIC FOR MONEY")
+	local am = math.random(Config.SellHorse.MinPrice,Config.SellHorse.MaxPrice)
+	if Config.Framework == "redemrp" then 
+			
+	elseif Config.Framework == "vorp" then 
+			
+	elseif Config.Framework == "qbr" then 
+		local User = exports['qbr-core']:GetPlayer(_source)
+		User.Functions.AddMoney("cash", 100, "desc")
+	end
 end)
